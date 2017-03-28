8c0f00cdd56042b55452639a546ab4407df9f39b
Artur B <abartosiak@interia.pl>
2017-03-28 23:55:54 +0200

    'V1'

---
+++ Test/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Artur B on 2017-03-28.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HEJ");
+        System.out.println("HEJ 1");
+    }
+}
