94b11f34c895e1b9d906a7add24ec0b6c4cad39a
Artur B <abartosiak@interia.pl>
2017-03-29 23:06:22 +0200

    'V2'

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HEJ");
         System.out.println("HEJ 1");
+        System.out.println("HEJ 2");
+        System.out.println("HEJ 3");
+        System.out.println("HEJ 4");
+        System.out.println("HEJ 5");
     }
 }
